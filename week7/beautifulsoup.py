8f71aa3821bc6bdc2a93a01c137d383689697a6f
jwang02 <playdoh888@hotmail.com>
2020-07-08 16:30:23 -0500

    Added Outline for the rest of class.

---
+++ week7/beautifulsoup.py
@@ -0,0 +1,9 @@
+"""
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+https://www.dataquest.io/blog/web-scraping-tutorial-python/
+
+https://www.digitalocean.com/community/tutorials/how-to-scrape-web-pages-with-beautiful-soup-and-python-3
+"""
+
+
