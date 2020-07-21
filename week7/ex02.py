0dd111e4db229cf64ad83f4c2fd64367715c970b
jwang02 <playdoh888@hotmail.com>
2020-07-21 15:38:30 -0500

    Added explaination to the oustanding questions we had during the week6 and week5.

--- week7/ex02.py
+++ week7/ex02.py
@@ -3,16 +3,17 @@ import csv
 from bs4 import BeautifulSoup
 
 
-f = csv.writer(open('z-artist-names.csv', 'w'))
+f = csv.writer(open('b-artist-names.csv', 'w'))
 f.writerow(['Name', 'Link'])
 
 pages = []
 
-for i in range(1, 5):
-    url = 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ' + str(i) + '.htm'
+for i in range(1, 52):
+    url = 'https://web.archive.org/web/20121002115827/http://www.nga.gov/collection/anB' + str(i) + '.htm'
     pages.append(url)
 
 
+i = 0
 for item in pages:
     page = requests.get(item)
     soup = BeautifulSoup(page.text, 'html.parser')
@@ -23,8 +24,11 @@ for item in pages:
     artist_name_list = soup.find(class_='BodyText')
     artist_name_list_items = artist_name_list.find_all('a')
 
+    print(f"Parsing: {item}")
     for artist_name in artist_name_list_items:
-        names = artist_name.contents[0]
-        links = 'https://web.archive.org' + artist_name.get('href')
+        if artist_name.contents:
+            names = artist_name.contents[0]
+            links = 'https://web.archive.org' + artist_name.get('href')
+            f.writerow([names, links])
 
-        f.writerow([names, links])
+    i += 1
